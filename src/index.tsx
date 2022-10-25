7cd5b655f97e52ef674e9c43f87233cf51985855
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2022-10-25 22:55:44 +0700

    Pre-Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from 'src/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const app = (
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+root.render(app);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
